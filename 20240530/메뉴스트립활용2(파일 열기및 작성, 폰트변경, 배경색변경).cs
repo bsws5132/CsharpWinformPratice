eabf38393d7ae8e1fbd6cd5412ff0db2e64949af
Half_dead <84064967+bsws5132@users.noreply.github.com>
2024-05-30 17:10:45 +0900

    Add files via upload

---
+++ 20240530/메뉴스트립활용2(파일 열기및 작성, 폰트변경, 배경색변경).cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+namespace WindowsFormsApp30
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 글꼴및크기변경ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            FontDialog fontD = new FontDialog();
+            
+            DialogResult result = fontD.ShowDialog();
+
+            richTextBox1.Font = fontD.Font;
+        }
+
+        private void 파일저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            
+            
+            saveFileDialog.Filter = "텍스트 파일 (*.txt)|*.txt|모든 파일 (*.*)|*.*";
+            DialogResult result = saveFileDialog.ShowDialog();
+
+            string fileName = saveFileDialog.FileName;
+            File.WriteAllText(fileName, richTextBox1.Text);
+            // 저장 대화상자에서 사용자가 입력한 파일명으로 리치박스내의 글을 파일로 저장
+        }
+
+        private void 배경색상변경ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            DialogResult dialogResult = colorDialog.ShowDialog();
+
+            richTextBox1.BackColor = colorDialog.Color;
+        }
+
+        private void 파일열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "텍스트 파일 (*.txt)|*.txt|모든 파일 (*.*)|*.*";
+
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+            string fileName = openFileDialog.FileName;
+            richTextBox1.Text = File.ReadAllText(fileName);
+
+        }
+
+        private void 초기화ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
---
+++ 20240530/툴팁, 클릭하면 클립보드 복사.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+         Clipboard.SetText(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox2.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(textBox1, textBox1.Text);
+            toolTip1.SetToolTip(textBox2, textBox2.Text);
+        }
+    }
+}
